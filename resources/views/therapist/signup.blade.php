73cd856e33ab8e06975c37620fa55c280e66b2ab
Al-sany Samil <alsanysamil@gmail.com>
2021-04-18 23:04:31 +0600

    initail commit

---
+++ resources/views/therapist/signup.blade.php
@@ -0,0 +1,18 @@
+{{-- <div dir="ltr" style="text-align: left;" trbidi="on">
+        <script>
+          (function(d, s, id) {
+            var js, ref = d.getElementsByTagName(s)[0];
+            if (!d.getElementById(id)) {
+              js = d.createElement(s);
+              js.id = id;
+              js.async = true;
+              js.src = "https://www.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js";
+              ref.parentNode.insertBefore(js, ref);
+            }
+          }(document, "script", "paypal-js"));
+      
+        </script>
+        <a data-paypal-button="true" href="<Action-URL>&displayMode=minibrowser" target="PPFrame">Sign up for PayPal</a>
+      </div>  --}}
+      {{-- <a href="/paypal-setup">Sign up for Paypal </a> --}}
+      <a href="https://www.sandbox.paypal.com/bizsignup/partner/entry?&partnerClientId=AbrYBqmnK90SagvriEV0A7XwxXhAsNszJyGnP-5lR50K60ylUj1esCQ2glmwJ51oRp7HY7W2kR5YS0-F&partnerId=5SQ6W5CKHRN4Y&partnerLogoUrl=https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_111x69.jpg&returnToPartnerUrl=localhost:8000/signup&product=ppcp&merchantId=1&showPermissions=true&integrationType=FO&features=PAYMENT,REFUND">Sign Up</a>
---
+++ routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Models\Info;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/signup', function () {
+    return view('therapist.signup');
+});
+
+Route::get('/paypal-setup', function(){
+    $user = Info::where('id', 1)->first();
+    $token = base64_encode(env('PAYPAL_CLIENT_ID').':'.env('PAYPAL_CLIENT_SECRET'));
+    $client = new Client();
+    $response = $client->request('POST', 'https://api-m.sandbox.paypal.com/v2/customer/partner-referrals', [
+        'headers' => [
+            'Content-Type'     => 'application/json',
+            'Authorization'      => 'Basic '.$token
+        ],
+        'form-params' => [
+            "tracking_id" => $user->id,
+            "email" => $user->email,
+            "partner_config_override" => [
+                "partner_logo_url" => "https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_111x69.jpg",
+                "return_url" => "localhost:8000/",
+                "return_url_description" => "the url to return the merchant after the paypal onboarding process.",
+                // "action_renewal_url" => "https://testenterprises.com/renew-exprired-url",
+                "show_add_credit_card" => true
+            ],
+            "operations" => [
+              [
+                "operation" => "API_INTEGRATION",
+                "api_integration_preference" => [
+                  "rest_api_integration" => [
+                    "integration_method" => "PAYPAL",
+                    "integration_type" => "THIRD_PARTY",
+                    "third_party_details" => [
+                      "features" => [
+                        "PAYMENT",
+                        "REFUND"
+                     ]
+                    ]
+                  ]
+                ]
+              ]
+            ],
+            "products" => [
+              "EXPRESS_CHECKOUT"
+            ],
+            "legal_consents" => [
+              [
+                "type" => "SHARE_DATA_CONSENT",
+                "granted" => true
+              ]
+            ]
+        ]
+    ]);
+    $links = json_decode($response->getBody());
+    return redirect($links->links[1]->href);
+});
